0ab606644fd4dc6e56fdb2c35da46caddaa3a60c
Omar Abdulkhalek <omar.abdulkhalek@amana.de>
2023-01-29 00:07:58 +0100

    customized Many-to-One relationship

--- src/main/java/com/abdulkhalekomar/entity/Salary.java
+++ src/main/java/com/abdulkhalekomar/entity/Salary.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -33,6 +34,7 @@ public class Salary
 	private String title;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "employee_id")
 	private Employee employee;
 
 	@Id
